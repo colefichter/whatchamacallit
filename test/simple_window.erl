ad3ce031b89b72f07486b0fb254dd0753cf7eb76
Cole Fichter <cole.fichter@gmail.com>
2015-10-08 19:38:57 -0600

    Adding some full examples to tests. Not sure if this is ideal.

---
+++ test/simple_window.erl
@@ -0,0 +1,15 @@
+-module(simple_window).
+
+-compile([export_all]).
+
+start() -> 
+    io:format("wtest:start ~p~n", [self()]),
+    w:new_frame("TESTING!").
+
+-include_lib("eunit/include/eunit.hrl").
+
+all_test() ->
+    w_server:start(),
+    start(),
+    timer:sleep(25),
+    w_server:stop().
---
+++ test/toolbar_statusbar.erl
@@ -0,0 +1,37 @@
+-module(toolbar_statusbar).
+
+-export([start/0, init/0]).
+
+start() -> spawn(?MODULE, init, []).
+
+init() -> 
+    Frame = w:new_frame("TESTING!", [{size, {200, 200}}]),
+    w:add_statusbar(Frame, "Statusbar text set quickly!"),
+    _Panel = w:new_panel(Frame),
+    ToolbarButtonDef = [
+        {"New", "wxART_NEW", "This is long help for 'New'"},
+        {"Press Me", "wxART_ERROR"},
+        {"Copy", "wxART_COPY", "Copy something to the clipboard"} %Long Help ends up in status bar!
+    ],
+    _Buttons = w:add_toolbar(Frame, ToolbarButtonDef),
+    w:show(Frame),
+    loop().
+
+loop() ->
+    receive
+        stop -> ok;
+        {click, {button, ButtonId, ButtonText}} ->
+            io:format("Handling button click: Id ~p Text ~p~n", [ButtonId, ButtonText]),
+            loop();
+        Message ->
+            io:format("CALLBACK INVOKED ~p: ~p~n", [self(), Message]),
+            loop()
+    end.
+
+-include_lib("eunit/include/eunit.hrl").
+
+all_test() ->
+    w_server:start(),
+    start(),
+    timer:sleep(25),
+    w_server:stop().
